22fa9e40d3f5249827a610f0bce49198a855689b
Semmieeeh <semleungen05@gmail.com>
2022-11-18 19:24:43 +0100

    Funny Input Context
    L

--- VRBoxing/Assets/Sem/Scripts/VRMovement.cs
+++ VRBoxing/Assets/Sem/Scripts/VRMovement.cs
@@ -2,23 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 
 
 public class VRMovement : MonoBehaviour
 {
-
-
-    public XRNode inputSource;
-    // Start is called before the first frame update
+    
+         
     void Start()
     {
+       
         
     }
 
     // Update is called once per frame
-    void Update()
+    public void Movement(InputAction.CallbackContext context)
     {
-        
+        Vector2 moveDir = context.ReadValue<Vector2>();
+        print(moveDir);
+        print("Moving");
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        Vector3 lookDir = context.ReadValue<Vector2>();
+        print(lookDir);
+        print("Looking");
+    }
+
+    public void HardenFist(InputAction.CallbackContext context)
+    {
+        float axis = context.ReadValue<float>();
+        print(axis);
+        print("Hardened");
+    }
+    public void ReleaseHarden(InputAction.CallbackContext context)
+    {
+        float axis = context.ReadValue<float>();
+        print(axis);
+        print("UnHardened");
+    }
+    public void Grab(InputAction.CallbackContext context)
+    {
+        float axis = context.ReadValue<float>();
+        print(axis);
+        print("Grabbed");
+    }
+    public void ReleaseGrab(InputAction.CallbackContext context)
+    {
+        float axis = context.ReadValue<float>();
+        print(axis);
+        print("Released Grab");
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        float axis = context.ReadValue<float>();
+        print(axis);
+        print("Jumped");
     }
 }
