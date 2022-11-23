35174d9ce62757f18371f29068ee2495d4a2e01f
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2022-11-23 14:22:05 +0100

    main menu update

--- VRBoxing/Assets/MainMenuManager.cs
+++ VRBoxing/Assets/MainMenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
+using Oculus.Interaction.PoseDetection;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -12,17 +13,29 @@ public class MainMenuManager : MonoBehaviour
     [Tooltip("All The animators that get triggered whenever the play button is pressed on the start screen")]
     public AnimatorData[] gameStartAnimators;
 
+    [Tooltip("All the animators that will get triggered whenever the player goes to a submenu")]
+    public AnimatorData[] baseDisableAnimators;
+
     [Header("Menu Pages")]
     public MenuPage[] menuPages;
 
     public float pageSwitchWaitTime;
 
+    [Header("Options Settings")]
+    public Animator[] lineAnimators;
+    public float lineToggleTime;
+
+    public AnimatorData[] optionsEnableAnimators;
+    public AnimatorData[] optionsDisableAnimators;
+
+    public Animator optionsTabAnimator;
+    public GameObject video, gameplay, _audio;
+
     [Header("Credits Settings")]
-    [Header("Enter")]
-    public AnimatorData[] creditsDisableAnimators;
+
 
     public AnimatorData[] creditsEnableAnimators;
-    [Header("Exit")]
+
     public AnimatorData[] creditsExitAnimators;
 
     /// <summary>
@@ -37,14 +50,15 @@ public class MainMenuManager : MonoBehaviour
         }
     }
 
+    #region Credits
     /// <summary>
     /// Function called whenever the player pressed the Credits button in the main menu
     /// </summary>
     public async void OnCreditsEnter()
     {
-        for (int i = 0; i < creditsDisableAnimators.Length; i++)
+        for (int i = 0; i < baseDisableAnimators.Length; i++)
         {
-            creditsDisableAnimators[i].Start();
+            baseDisableAnimators[i].Start();
         }
 
         await Task.Delay(ToMilliseconds(pageSwitchWaitTime));
@@ -73,6 +87,114 @@ public class MainMenuManager : MonoBehaviour
         }
 
     }
+    #endregion
+
+    #region Options
+
+    /// <summary>
+    /// Function called whenever the player pressed the Options button in the main menu
+    /// </summary>
+    public async void OnOptionEnter()
+    {
+        for (int i = 0; i < baseDisableAnimators.Length; i++)
+        {
+            baseDisableAnimators[i].Start();
+        }
+
+        await Task.Delay(ToMilliseconds(pageSwitchWaitTime));
+
+        OpenPage("Options",true);
+
+        ToggleOptionsSubMenu(1);
+
+        ToggleOptionsLine(true);
+
+        for (int i = 0; i < optionsEnableAnimators.Length; i++)
+        {
+            optionsEnableAnimators[i].Start();
+        }
+
+    }
+
+    /// <summary>
+    /// Function called whenever the player pressed the Back button in the Options menu
+    /// </summary>
+    public async void OnOptionsExit()
+    {
+        for (int i = 0; i < optionsDisableAnimators.Length; i++)
+        {
+            optionsDisableAnimators[i].Start();
+        }
+
+        ToggleOptionsLine(false);
+
+        ToggleOptionsSubMenu(-1);
+
+        await Task.Delay(ToMilliseconds(pageSwitchWaitTime + lineToggleTime * lineAnimators.Length));
+
+        OpenPage("Main Screen", true);
+
+        for (int i = 0; i < gameStartAnimators.Length; i++)
+        {
+            gameStartAnimators[i].Start();
+        }
+
+    }
+
+    /// <summary>
+    /// Toggles the Animation of the line in the Options menu
+    /// </summary>
+    /// <param name="enable"></param>
+    public async void ToggleOptionsLine(bool enable)
+    {
+        foreach (var animator in lineAnimators)
+        {
+            animator.SetBool("Enabled", enable);
+            await Task.Delay(ToMilliseconds(lineToggleTime));
+        }
+    }
+
+    int previousIndex;
+    bool changing;
+    public async void ToggleOptionsSubMenu(int menuIndex)
+    {
+        if (previousIndex == menuIndex || changing) return;
+        changing = true;
+
+        optionsTabAnimator.SetBool("Toggle",false);
+
+        await Task.Delay(ToMilliseconds(0.25f));
+
+
+        switch (menuIndex)
+        {
+            case (int)OptionsMenu.Video:
+                video.SetActive(true);
+                gameplay.SetActive(false);
+                _audio.SetActive(false);
+                break;
+            case (int)OptionsMenu.Gameplay:
+                video.SetActive(false);
+                gameplay.SetActive(true);
+                _audio.SetActive(false);
+                break;
+            case (int)OptionsMenu.Audio:
+                video.SetActive(false);
+                gameplay.SetActive(false);
+                _audio.SetActive(true);
+                break;
+            default:
+                video.SetActive(false);
+                gameplay.SetActive(false);
+                _audio.SetActive(false);
+                break;
+        }
+        optionsTabAnimator.SetBool("Toggle", true);
+        previousIndex = menuIndex;
+        changing = false;
+    }
+
+    #endregion 
 
     void ClosePage() => OpenPage("",true);
     
@@ -97,7 +219,7 @@ public class MainMenuManager : MonoBehaviour
                 menuPages[i].gameObject.SetActive(false);
         }
     }
-    
+
     public int ToMilliseconds(float value)
     {
         return Mathf.RoundToInt(value * 1000);
@@ -108,6 +230,13 @@ public class MainMenuManager : MonoBehaviour
     {
         public GameObject gameObject;
         public string pageName;
+    }
+
+    public enum OptionsMenu
+    {
+        Video,
+        Gameplay,
+        Audio
     }
 }
 
