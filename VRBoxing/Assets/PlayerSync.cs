b04849fcfc497c03825672093107a3d16d985385
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2022-12-20 19:35:23 +0100

    Revert "EEEH EH EH EH"
    This reverts commit 097e9b6b25d4bf124c739d0a94c90d357ea0e0ba.

--- VRBoxing/Assets/PlayerSync.cs
+++ VRBoxing/Assets/PlayerSync.cs
@@ -1,47 +1,38 @@
 using UnityEngine;
 using Photon.Pun;
-using System.Collections.Generic;
 
 public class PlayerSync : MonoBehaviourPun
 {
-    #region Player Body Parts
-
-    [Header("Body Parts")]
+    // The transform component of the player's body
     public Transform bodyTransform;
 
+    // The transform component of the player's left hand
     public Transform leftHandTransform;
 
+    // The transform component of the player's right hand
     public Transform rightHandTransform;
 
+    // The current position of the player's body
     private Vector3 bodyPosition;
 
+    // The current rotation of the player's body
     private Quaternion bodyRotation;
 
+    // The current position of the player's left hand
     private Vector3 leftHandPosition;
 
+    // The current rotation of the player's left hand
     private Quaternion leftHandRotation;
 
+    // The current position of the player's right hand
     private Vector3 rightHandPosition;
 
+    // The current rotation of the player's right hand
     private Quaternion rightHandRotation;
 
-    #endregion
-
-    #region Animators
-
-    [Header("Animators")]
-    public Animator leftHandAnimator;
-
-    public Animator rightHandAnimator;
-
-    public string[] leftHandAnimatorParameterNames;
-
-    public string[] rightHandAnimatorParameterNames;
-    #endregion
-
-    void FixedUpdate()
+    void Update()
     {
-        // Dont update the players data if we are the master client
+        // If this script is not being executed on the master client, do not update the player's body and hand transforms
         if (!photonView.IsMine)
         {
             return;
@@ -59,89 +50,14 @@ public class PlayerSync : MonoBehaviourPun
         photonView.RPC(nameof(SyncBodyPart), RpcTarget.Others, bodyTransform, bodyPosition, bodyRotation);
         photonView.RPC(nameof(SyncBodyPart), RpcTarget.Others, leftHandTransform, leftHandPosition, leftHandRotation);
         photonView.RPC(nameof(SyncBodyPart), RpcTarget.Others, rightHandTransform, rightHandPosition, rightHandRotation);
-
-        //Get the current Parameters of the players hand animators
-
-        AnimatorParameters[] leftHandParameters = new AnimatorParameters[]
-        {
-            new AnimatorParameters
-            {
-                paramName = leftHandAnimatorParameterNames[0],
-                value = leftHandAnimator.GetBool(leftHandAnimatorParameterNames[0]),
-                valueType = AnimatorValueType.Bool
-            }
-        };
-
-        AnimatorParameters[] rightHandParameters = new AnimatorParameters[]
-        {
-            new AnimatorParameters
-            {
-                paramName = rightHandAnimatorParameterNames[0],
-                value = rightHandAnimator.GetBool(rightHandAnimatorParameterNames[0]),
-                valueType = AnimatorValueType.Bool
-            }
-        };
-
-        //Set the animators parameters on the photon server
-
-        photonView.RPC(nameof(SyncAnimators), RpcTarget.Others, leftHandAnimator, leftHandParameters);
-        photonView.RPC(nameof(SyncAnimators), RpcTarget.Others, rightHandAnimator, rightHandParameters);
     }
 
-    public enum AnimatorValueType
-    {
-        Int,
-        Float,
-        Bool,
-        Trigger,
-    }
-    public struct AnimatorParameters
-    {
-        public string paramName;
-        public object value;
-        public AnimatorValueType valueType;
-    }
 
-    #region RPCMethods;
     //RPC method to synchronize objects across the photon network
     [PunRPC]
     public void SyncBodyPart(Transform bodyPart, Vector3 pos, Quaternion rot)
     {
-        if (!Application.isPlaying) return;
-
         bodyPart.position = pos;
         bodyPart.rotation = rot;
     }
-
-    //RPC Method to synchronize animators across the photon network
-    [PunRPC]
-    public void SyncAnimators(Animator animator, params AnimatorParameters[] parameters )
-    {
-        if (!Application.isPlaying) return;
-
-
-        for (int i = 0; i < parameters.Length; i++)
-        {
-            var parameter = parameters[i];
-
-            switch (parameter.valueType)
-            {
-                case AnimatorValueType.Int:
-                    animator.SetInteger(parameter.paramName, (int)parameter.value);
-                    break;
-                case AnimatorValueType.Float:
-                    animator.SetFloat(parameter.paramName, (float)parameter.value);
-                    break;
-                case AnimatorValueType.Bool:
-                    animator.SetBool(parameter.paramName, (bool)parameter.value);
-                    break;
-                case AnimatorValueType.Trigger:
-                    animator.SetTrigger(parameter.paramName);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-    #endregion
 }
