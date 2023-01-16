e0a5b16ed805cfd4eed7f6d777fe68d8eb9575e6
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2023-01-16 15:24:58 +0100

    Update Server.cs

--- VRBoxing/Assets/Server.cs
+++ VRBoxing/Assets/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class Server : MonoBehaviourPunCallbacks
 {
@@ -24,8 +25,47 @@ public class Server : MonoBehaviourPunCallbacks
         }
         print(MyPlayer);
         print(OtherPlayer);
+        ManageMyPlayer();
     }
 
     public static Player MyPlayer;
     public static Player OtherPlayer;
+
+    void ManageMyPlayer()
+    {
+        Hashtable properties = MyPlayer.CustomProperties;
+        if (properties.ContainsKey("DMGA"))
+        {
+            if (!(bool)properties["DMGA"])
+            {
+                print("You took" + (float)properties["DMG"] + " damage");
+                properties["DMGA"] = true;
+            }
+        }
+
+        MyPlayer.SetCustomProperties(properties);
+    }
+    public static void DamageEnemy(float damage)
+    {
+        Hashtable properties = OtherPlayer.CustomProperties;
+        if (!properties.ContainsKey("DMG"))
+        {
+            properties.Add("DMG", damage);
+            //{ "DMG" , 35 }
+        }
+        else
+        {
+            properties["DMG"] = damage;
+        }
+        if (!properties.ContainsKey("DMGA"))
+        {
+            properties.Add("DMGA", false);
+        }
+        else
+        {
+            properties["DMGA"] = false;
+        }
+
+        OtherPlayer.SetCustomProperties(properties);
+    }
 }
