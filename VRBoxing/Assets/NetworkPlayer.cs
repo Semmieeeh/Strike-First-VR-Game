da6c7324acf2b14af092b44c12202946a5fede36
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2023-01-16 10:55:28 +0100

    213

--- VRBoxing/Assets/NetworkPlayer.cs
+++ VRBoxing/Assets/NetworkPlayer.cs
@@ -7,6 +7,8 @@ public class NetworkPlayer : MonoBehaviour
 {
     // Reference to the PhotonView component.
     public PhotonView photonView;
+    public PhotonView leftHandPhotonView;
+    public PhotonView rightHandPhotonView;
 
     // References to the head and hand transforms.
     public Transform headTransform;
@@ -49,8 +51,8 @@ public class NetworkPlayer : MonoBehaviour
             // This is the local player's network player.
             // Synchronize the head and hand transforms with the local player's camera and hand transforms.
             photonView.RPC(nameof(MapHeadPosition), RpcTarget.Others, localCameraTransform.position, localCameraTransform.rotation);
-            photonView.RPC(nameof(MapLeftHandPosition), RpcTarget.Others,  localLeftHandTransform.position, localLeftHandTransform.rotation);
-            photonView.RPC(nameof(MapRightHandPosition), RpcTarget.Others, localRightHandTransform.position, localRightHandTransform.rotation);
+            leftHandPhotonView.RPC(nameof(MapLeftHandPosition), RpcTarget.Others,  localLeftHandTransform.position, localLeftHandTransform.rotation);
+            rightHandPhotonView.RPC(nameof(MapRightHandPosition), RpcTarget.Others, localRightHandTransform.position, localRightHandTransform.rotation);
         }
     }
 
