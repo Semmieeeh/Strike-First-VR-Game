d96ca0ed82dac745fe0eb39f269c7cf8ba6558c5
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2023-01-23 16:23:24 +0100

    begin gameplay

--- VRBoxing/Assets/InGameDisplay.cs
+++ VRBoxing/Assets/InGameDisplay.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 using Photon.Pun;
 using TMPro;
 using Photon.Realtime;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class InGameDisplay : MonoBehaviourPunCallbacks
 {
     [Header("Pre-Game")]
     public TextMeshProUGUI waitingTMP;
+
+    public TextMeshProUGUI player1, player2;
+    public float timeToWaitWhenGameCanStart;
+
+    public int roundsToPlay;
 
     Room currentRoom;
 
@@ -22,6 +29,8 @@ public class InGameDisplay : MonoBehaviourPunCallbacks
 
     bool gameStarted;
     bool prepareGameStarted;
+
+    public bool playerDied;
     public void Update()
     {
         // Only the client of the host will update the in-game display. the other client(s) will fetch the data of the in-game display
@@ -44,10 +53,34 @@ public class InGameDisplay : MonoBehaviourPunCallbacks
         {
             PrepareStartGame();
         }
+
+        //update the players name
+        if (Server.MyPlayer != null)
+        {
+            player1.text = Server.MyPlayer.NickName;
+        }
+        if(Server.OtherPlayer != null)
+        {
+            player2.text = Server.OtherPlayer.NickName;
+        }
     }
 
     void PrepareStartGame()
     {
-
+        print("Game Can Start!");
+        timeToWaitWhenGameCanStart -= Time.deltaTime;
+        if(timeToWaitWhenGameCanStart <= 0)
+        {
+            StartGame();
+        }
     }
+
+    void StartGame()
+    {
+        for (int i = 0; i < roundsToPlay; i++)
+        {
+
+        }
+    }
+
 }
