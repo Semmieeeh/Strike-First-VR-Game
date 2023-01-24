bf7de41491a9bddf4532c08ce435b31e7986608d
Semmieeeh <semleungen05@gmail.com>
2023-01-24 18:42:27 +0100

    Rotation meuk

--- VRBoxing/Assets/FollowHead.cs
+++ VRBoxing/Assets/FollowHead.cs
@@ -7,7 +7,9 @@ public class FollowHead : MonoBehaviour
     public Transform target;
     public GameObject head;
     private Rigidbody rb;
-    public bool isLeft;
+    public float rotDif;
+    
+    public bool isPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +25,14 @@ public class FollowHead : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Position();
-        Rotation();
+        if(isPlayer == false)
+        {
+            Position();
+        }
+        else if (isPlayer == true)
+        {
+            Rotation();
+        }
         
     }
     public void Position()
@@ -37,13 +45,44 @@ public class FollowHead : MonoBehaviour
     }
     public void Rotation()
     {
-        Quaternion headRot = target.rotation;
-        if (headRot.z - gameObject.transform.rotation.z < -30f || headRot.z- gameObject.transform.rotation.z > 30f)
+        //Quaternion myRot = gameObject.transform.rotation;
+        //Quaternion headRot = head.transform.rotation;
+        //float threshHold = 30f;
+        //// 90-0 < 30
+        //// 90+0 >-30
+        //if(headRot.y + myRot.y > threshHold)
+        //{
+        //    myRot.y = headRot.y;
+        //    myRot.x = gameObject.transform.rotation.x;
+        //    myRot.z = gameObject.transform.rotation.z;
+
+        //    gameObject.transform.rotation = myRot;
+        //    rotDif = headRot.y + myRot.y;
+        //}
+        //else if (headRot.y + myRot.y > -threshHold)
+        //{
+
+        //    myRot.y = headRot.y;
+        //    myRot.x = gameObject.transform.rotation.x;
+        //    myRot.z = gameObject.transform.rotation.z;
+
+        //    gameObject.transform.rotation = myRot;
+        //    rotDif = headRot.y + myRot.y;
+        //}
+
+
+        Quaternion myRot = gameObject.transform.rotation;
+        Quaternion headRot = head.transform.rotation;
+        float threshold = 45f;
+        float angle = Quaternion.Angle(myRot, headRot);
+        rotDif = angle;
+        if(angle > threshold)
         {
-            headRot.z = target.rotation.z - 30f;
-            headRot.y = gameObject.transform.rotation.y;
-            headRot.x = gameObject.transform.rotation.x;
-            gameObject.transform.rotation = headRot;
+            Debug.Log("Rotating");
+            Quaternion newRot = Quaternion.RotateTowards(transform.rotation, head.transform.rotation, 5*sTime.deltaTime);
+            myRot.y = newRot.y;
+            transform.rotation = myRot;
         }
+        
     }
 }
