18901cabe40f655e272ce6c6f63cf6c522b756cf
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2023-01-25 15:37:57 +0100

    q4ewardsfc

--- VRBoxing/Assets/InGameDisplay.cs
+++ VRBoxing/Assets/InGameDisplay.cs
@@ -239,7 +239,7 @@ public class InGameDisplay : MonoBehaviourPunCallbacks
         var MyPlayerProperties = Server.MyPlayer.CustomProperties;
         var OtherPlayerProperties = Server.OtherPlayer.CustomProperties;
 
-        if ((float)MyPlayerProperties[Server.kHealth] <= 0)
+        if ((float)MyPlayerProperties[Server.kHealth] >= 0)
         {
             return Server.MyPlayer;
         }
@@ -283,14 +283,14 @@ public class InGameDisplay : MonoBehaviourPunCallbacks
         //Update Player 1 settings
         var player1Properties = Server.MyPlayer.CustomProperties;
         player1InGame.text = Server.MyPlayer.NickName;
-        player1Health.text = ((float)player1Properties[Server.kHealth]).ToString("D") + "%";
-        player1RoundsWon.text = ((float)player1Properties[Server.kHealth]).ToString("D") + " Rounds Won";
+        player1Health.text = (Mathf.RoundToInt((float)player1Properties[Server.kHealth])).ToString() + "%";
+        player1RoundsWon.text = (Mathf.RoundToInt((float)player1Properties[Server.kHealth])).ToString() + " Rounds Won";
 
         //Update Player 2 settings
         var player2Properties = Server.OtherPlayer.CustomProperties;
         player2InGame.text = Server.OtherPlayer.NickName;
-        player2Health.text = ((int)player2Properties[Server.kHealth]).ToString() + "%";
-        player2RoundsWon.text = ((int)player2Properties[Server.kRoundsWon]).ToString() + "Rounds Won";
+        player2Health.text = (Mathf.RoundToInt((float)player2Properties[Server.kHealth])).ToString() + "%";
+        player2RoundsWon.text = (Mathf.RoundToInt((float)player2Properties[Server.kHealth])).ToString() + "Rounds Won";
 
         //Update Trophy Picture
         int player1Wins = (int)player1Properties[Server.kRoundsWon];
